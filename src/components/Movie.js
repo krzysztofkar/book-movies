65875ecfbe6c170af093934c0b15cf8cadb1931c
krzysztofkar <krzysztofkarczewski92@gmail.com>
2019-12-28 13:01:51 +0100

    add single movie view with Router (#1)

---
+++ src/components/Movie.js
@@ -0,0 +1,54 @@
+import React from "react";
+import moviesAPI from "../api/movies-api";
+import { Link } from "react-router-dom";
+class Movie extends React.Component {
+  state = { movie: [] };
+
+  componentDidMount = async () => {
+    // Fetching movie by its ID passed via props
+    const {
+      match: { params }
+    } = this.props;
+    const response = await moviesAPI.get(`/v4/movies/${params.id}`, {});
+    this.setState({ movie: response.data.movie });
+  };
+
+  render() {
+    return (
+      <div className="ui items">
+        <Link to={`/`}>
+          <button className="ui button"> Go back to movies </button>
+        </Link>
+        <div className="item" style={{ maxWidth: "60%" }}>
+          <div className="image">
+            <img
+              alt="movie-poster"
+              src={this.state.movie.poster_image_thumbnail}
+            />
+          </div>
+          <div className="content">
+            <h4 className="header">{this.state.movie.title}</h4>
+            <div className="meta">
+              <span className="cinema">{this.state.movie.runtime} minutes</span>
+            </div>
+            <hr />
+            <div className="description">
+              <p>{this.state.movie.synopsis}</p>
+              <br />
+            </div>
+            <a href={this.state.movie.website} target="_blank">
+              More info
+            </a>
+          </div>
+          <div className="extra">
+            <div className="ui right floated primary button">
+              Buy tickets
+              <i className="right chevron icon"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Movie;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,20 +1,32 @@
 import React from "react";
+import Movie from "./Movie";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const MovieList = props => {
   const movies = props.movies.map(movie => {
     if (movie.poster_image_thumbnail && movie.title) {
       return (
-        <div className="ui card" style={{ width: "200px" }}>
+        <div className="ui card" style={{ width: "200px" }} key={movie.id}>
           <div className="image">
             <img alt="movie-poster" src={movie.poster_image_thumbnail} />
           </div>
-          <div className="content">{movie.title}</div>
-          <button className="ui button">Book tickets!</button>
+          <Link to={`/movies/${movie.id}`}>
+            <div className="content">{movie.title}</div>
+          </Link>
         </div>
       );
     }
   });
-  return <div className="ui cards">{movies}</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movies/:id" component={Movie} />
+        <Route path="/">
+          <div className="ui cards">{movies}</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default MovieList;
