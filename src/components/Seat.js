913e02a2342f5b5307c4b8f734049b96de52107b
Emilia <jacaszek.emilia@gmail.com>
2020-01-05 18:33:33 +0100

    form connected to state

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -2,22 +2,25 @@ import React from 'react';
 import './Seating.css';
 
 class Seat extends React.Component {
-    state = { chosen: false };
+  state = { chosen: false, id: null };
 
   toggleChoose = () => {
     const currentState = this.state.chosen;
-    this.setState({ chosen: !currentState });
+    const currentItem = this.props.number;
+    this.setState({ chosen: !currentState, id: currentItem }, () => {
+      this.props.handleSelectSeat(this.state);
+    });
   };
 
   render() {
     return (
     <div className="Seat"
-    onClick={this.toggleChoose}
-    style={{
-        backgroundColor:this.state.chosen ? "rgb(0, 128, 192)" : "rgb(122, 122, 122)"
-    }}
+      onClick={this.toggleChoose}
+      style={{
+          backgroundColor:this.state.chosen ? "rgb(0, 128, 192)" : "rgb(122, 122, 122)"
+      }}
     >
-        <div className ="each-seat">{this.props.number}</div>
+      <div className ="each-seat">{this.props.number}</div>
     </div>
     );
 }
--- src/components/Seating.js
+++ src/components/Seating.js
@@ -3,18 +3,46 @@ import { Link } from "react-router-dom";
 import Seat from './Seat';
 
 class Seating extends Component {
+  state = {
+    seats: []
+  }
+
+    handleSubmit = () => {
+      console.log(this.state)
+      return this.state;
+    }
+
+    handleChange = (e) => {
+      const value = e.target.value;
+      const name = e.target.name;
+      this.setState({
+        [name]: value
+      })
+    }
+
+    handleSelectSeat = (newSeat) => {
+      if (newSeat.chosen) {
+        this.state.seats.push(newSeat.id)
+      } else {
+        this.setState({
+          seats: this.state.seats.filter(seat => seat.id !== newSeat.id)
+        })
+      }
+    }
+
   render() {
     var rows = [];
     for (var i = 1; i <= 30; i++) {
     rows.push(<Seat
     number={i}
+    key={i}
+    handleSelectSeat={this.handleSelectSeat}
     ></Seat>);
   }
-
     return (
     <div className="container-seats">
     <Link to={`/`}>
-          <button className="ui button"> Go back to movies </button>
+          <button className="ui button back-btn"> Go back to movies </button>
         </Link>
         <div className="seating-wrapper">
       <div className="screen">
@@ -25,20 +53,29 @@ class Seating extends Component {
       <form className="form-wrapper">
         <h2>Fill this and book your seats</h2>
         <div className="info">
-        <input type="text" name="field1" placeholder="Your Name *"/>
-        <input type="text" name="field1" placeholder="Your Surname *"/>
-        <input type="email" name="field2" placeholder="Your Email *"/>
-        <input type="text" name="field1" placeholder="Your Phone *"/>
+        <input type="text" name="name" placeholder="Your Name *"
+          onChange={this.handleChange}
+        />
+        <input type="text" name="surname" placeholder="Your Surname *"
+          onChange={this.handleChange}
+        />
+        <input type="email" name="email" placeholder="Your Email *"
+          onChange={this.handleChange}
+        />
+        <input type="text" name="phone" placeholder="Your Phone *"
+          onChange={this.handleChange}
+        />
         </div>
-      </form>
       <div className="submit-btn">
       <button
-        type="submit"
+        type="button"
         className="ui inverted primary button book-btn"
+        onClick={this.handleSubmit}
       >
         Book those seats
       </button>
       </div>
+      </form>
     </div>
    
     );
