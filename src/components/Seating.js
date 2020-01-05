f3c3dc30ac0b26b1970a3e89f68cd5c0b11e3cfa
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-01-05 22:53:29 +0100

    fix routing

--- src/components/Seating.js
+++ src/components/Seating.js
@@ -1,86 +1,96 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Seat from './Seat';
+import Seat from "./Seat";
 
 class Seating extends Component {
   state = {
     seats: []
-  }
+  };
 
-    handleSubmit = () => {
-      console.log(this.state)
-      return this.state;
-    }
+  handleSubmit = () => {
+    console.log(this.state);
+    return this.state;
+  };
 
-    handleChange = (e) => {
-      const value = e.target.value;
-      const name = e.target.name;
+  handleChange = e => {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSelectSeat = newSeat => {
+    if (newSeat.chosen) {
+      this.state.seats.push(newSeat.id);
+    } else {
       this.setState({
-        [name]: value
-      })
+        seats: this.state.seats.filter(seat => seat.id !== newSeat.id)
+      });
     }
-
-    handleSelectSeat = (newSeat) => {
-      if (newSeat.chosen) {
-        this.state.seats.push(newSeat.id)
-      } else {
-        this.setState({
-          seats: this.state.seats.filter(seat => seat.id !== newSeat.id)
-        })
-      }
-    }
+  };
 
   render() {
     var rows = [];
     for (var i = 1; i <= 30; i++) {
-    rows.push(<Seat
-    number={i}
-    key={i}
-    handleSelectSeat={this.handleSelectSeat}
-    ></Seat>);
-  }
+      rows.push(
+        <Seat
+          number={i}
+          key={i}
+          handleSelectSeat={this.handleSelectSeat}
+        ></Seat>
+      );
+    }
     return (
-    <div className="container-seats">
-    <Link to={`/`}>
+      <div className="container-seats">
+        <Link to={`/`}>
           <button className="ui button back-btn"> Go back to movies </button>
         </Link>
         <div className="seating-wrapper">
-      <div className="screen">
-        SCREEN
-      </div>
-      <div className="rows">{rows}</div>
-      </div>
-      <form className="form-wrapper">
-        <h2>Fill this and book your seats</h2>
-        <div className="info">
-        <input type="text" name="name" placeholder="Your Name *"
-          onChange={this.handleChange}
-        />
-        <input type="text" name="surname" placeholder="Your Surname *"
-          onChange={this.handleChange}
-        />
-        <input type="email" name="email" placeholder="Your Email *"
-          onChange={this.handleChange}
-        />
-        <input type="text" name="phone" placeholder="Your Phone *"
-          onChange={this.handleChange}
-        />
+          <div className="screen">SCREEN</div>
+          <div className="rows">{rows}</div>
         </div>
-      <div className="submit-btn">
-      <button
-        type="button"
-        className="ui inverted primary button book-btn"
-        onClick={this.handleSubmit}
-      >
-        Book those seats
-      </button>
+        <form className="form-wrapper">
+          <h2>Fill this and book your seats</h2>
+          <div className="info">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name *"
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="surname"
+              placeholder="Your Surname *"
+              onChange={this.handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email *"
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Your Phone *"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="submit-btn">
+            <button
+              type="button"
+              className="ui inverted primary button book-btn"
+              onClick={this.handleSubmit}
+            >
+              Book those seats
+            </button>
+          </div>
+        </form>
       </div>
-      </form>
-    </div>
-   
     );
   }
 }
-
 
 export default Seating;
